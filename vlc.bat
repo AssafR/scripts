7dc00ab7c02416dc7bd43ef19d73fed4c939d7df
AssafR <aU2nQune>
2026-04-08 14:19:45 +0300

    Fixed vlc script

--- vlc.bat
+++ vlc.bat
@@ -1,2 +1,4 @@
 @echo off
-"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8"
+REM "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8"
+REM "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC media player.lnk" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8"
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8"
